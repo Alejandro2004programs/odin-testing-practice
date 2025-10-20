a94fd07a67221fc7ad723a92cffc671b4347e24d
Alejandro Alvarez <alejandroalvarez1011204@gmail.com>
2025-10-20 16:02:04 -0700

    wrote tests for capitalize function

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -1,1 +1,10 @@
 import reverseString from "../src/reverseString.js";
+
+test("simple reverse of a string", () => {
+    expect(reverseString("reverse")).toMatch("esrever");
+});
+
+test("simple reverse of a string that is the same backwards", () => {
+    expect(reverseString("racecar")).toMatch("racecar");
+});
+
