7d861f98e4ccb1227d37988e429b87ab582ae68b
Alejandro Alvarez <alejandroalvarez1011204@gmail.com>
2025-10-20 16:53:34 -0700

    finished calculator object that passes all the tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,31 @@
 class calculator {
-    sum(a, b) {
-
+    add(a, b) {
+        if(typeof a == "number" && typeof b == "number") {
+            return a + b;
+        }
+        else {
+            throw new Error();
+        }
     }
     subtract(a, b) {
-
+        if(typeof a == "number" && typeof b == "number") {
+            return a - b;
+        }
+        else {
+            throw new Error();
+        }
     }
     divide(a , b) {
-
+        if(b == 0) {
+            throw new Error();
+        }
+        else if(typeof a == "number" && typeof b == "number") {
+            return a / b;
+        }
+        else {
+            throw new Error();
+        }
+        
     }
 }
 
