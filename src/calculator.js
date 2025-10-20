3e499f7962e4bd32f328f48aa40aa10916e27cc1
Alejandro Alvarez <alejandroalvarez1011204@gmail.com>
2025-10-20 16:28:29 -0700

    wrote tests for calculator object functions

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+class calculator {
+    sum(a, b) {
+
+    }
+    subtract(a, b) {
+
+    }
+    divide(a , b) {
+
+    }
+}
+
+export default calculator = new calculator();
+
---
+++ tests/calculator.test.js
@@ -0,0 +1,31 @@
+import calculator from "../src/calculator.js";
+
+test("simple addition of two numbers", () => {
+    expect(calculator.add(1,2)).toBe(3);
+})
+
+test("addition of non numbers", () => {
+    expect(calculator.add("a", 1)).toThrow();
+});
+
+test("simple subtraction of two numbers", () => {
+    expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test("subtraction of non numbers", () => {
+    expect(calculator.subtract("a", 1)).toThrow();
+});
+
+test("division of two non zero numbers", () => {
+    expect(calculator.division(4, 2)).toBe(2);
+});
+
+test("division by zero", () => {
+    expect(calculator.divide(5, 0)).toThrow(/No division by zero/);
+});
+
+test("division by non numbers", () => {
+    expect(calculator.divide("a", 1)).toThrow();
+});
+
+
